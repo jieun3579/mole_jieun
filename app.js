23cc422fe90e9e1c2cef89ecd0e1120dad6c2c4c
jieun <se7eniis2@gmail.com>
2025-12-10 13:45:12 +0900

    up 1210

---
+++ app.js
@@ -0,0 +1,52 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
+
+let timeId = null;
+let hitposition;
+let result = 0;
+let currentTime = 60;
+
+function randomSquare() {
+    squares.forEach((square) => {
+        square.classList.remove("mole");
+    });
+
+    let randomSquare = squares[Math.floor(Math.random()*9)];
+    randomSquare.classList.add("mole");
+    hitposition = randomSquare.id;
+}
+
+
+squares.forEach((square) => {
+    square.addEventListener("mousedown",() =>{
+        if(square.id == hitposition) {
+            result++;
+            score.textContent = result;
+            hitposition=null;
+        }
+    });
+});
+
+
+function moveMole() {
+    timeId = setInterval(randomSquare,500);
+}
+
+
+moveMole();
+
+
+function countDown() {
+    currentTime--;
+    timeLeft.textContent = currentTime;
+    if(currentTime == 0) {
+        clearInterval(countDownTimeId);
+        clearInterval(timeId);
+        alert("Game Over! Your final score is "+ result)
+    }
+}
+
+
+let countDownTimeId = setInterval(countDown,1000);
